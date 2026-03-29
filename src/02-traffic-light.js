21d5c1d210e12d462c650f481984281cd47a6f53
Shivraj <sr.rajput0101@gmail.com>
2026-03-29 13:55:01 +0530

    Solved 02 traffic light challenge

--- src/02-traffic-light.js
+++ src/02-traffic-light.js
@@ -22,5 +22,23 @@
  * @returns {string} The driving action to take
  */
 export function getTrafficAction(color) {
-  // Your code here
+  if (typeof color !== 'string') {
+    return "INVALID SIGNAL";
+  }
+  const safeColor = color.toLowerCase();
+  switch (safeColor) {
+    case 'green':
+      return "GO";
+      
+    case 'yellow':
+      return "SLOW DOWN";
+      
+    case 'red':
+      return "STOP";
+      
+    case 'flashing red':
+      return "STOP AND PROCEED WITH CAUTION";
+    default:
+      return "INVALID SIGNAL";
+  }
 }
